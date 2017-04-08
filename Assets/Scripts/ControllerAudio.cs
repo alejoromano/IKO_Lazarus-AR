7503e4a4c7d97eedcf636cbd795d25067cd007f4
GuilleLarosa <guille.larosaa@gmail.com>
2017-04-08 17:49:19 -0300

    iko kickoff

---
+++ Assets/Scripts/ControllerAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerAudio : MonoBehaviour {
+
+    public AudioSource audio1;
+    public GameObject triggerObj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (triggerObj.GetComponent<Collider>().enabled)
+        {
+            if (!audio1.isPlaying) audio1.Play();
+        }
+        else
+        {
+            if (audio1.isPlaying) audio1.Stop();
+        }
+	}
+}
